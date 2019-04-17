285c80b3e200869e8d520c119e494f8d1435f7c3
joy <joy.lai@zg.com>
2019-04-17 11:24:56 +0800

    improve

--- error.go
+++ error.go
@@ -1,10 +1,5 @@
 package p_error
 
-import (
-	"fmt"
-	"github.com/pefish/go-logger"
-)
-
 type ErrorInfo struct {
 	ErrorMessage string
 	ErrorCode uint64
@@ -12,11 +7,6 @@ type ErrorInfo struct {
 	Err error
 }
 
-var (
-	INTERNAL_ERROR = `internal error`
-
-	CODE_ERROR uint64 = 0
-)
 
 func (e *ErrorInfo) Error() string {
 	return e.ErrorMessage
@@ -25,37 +15,31 @@ func (e *ErrorInfo) Error() string {
 
 func ThrowInternal(text string) {
 	var errorInfo_ = ErrorInfo{text, 1, nil,nil}
-	p_logger.Logger.Error(fmt.Sprintf(`ERROR: %v`, errorInfo_))
 	panic(errorInfo_)
 }
 
 func ThrowInternalError(text string, err error) {
 	var errorInfo_ = ErrorInfo{text, 1, nil,err}
-	p_logger.Logger.Error(fmt.Sprintf(`ERROR: %v`, errorInfo_))
 	panic(errorInfo_)
 }
 
 func Throw(text string, code uint64) {
 	var errorInfo_ = ErrorInfo{text, code, nil,nil}
-	p_logger.Logger.Error(fmt.Sprintf(`ERROR: %v`, errorInfo_))
 	panic(errorInfo_)
 }
 
 func ThrowWithData(text string, code uint64, data interface{}) {
 	var errorInfo_ = ErrorInfo{text, code, data,nil}
-	p_logger.Logger.Error(fmt.Sprintf(`ERROR: %v`, errorInfo_))
 	panic(errorInfo_)
 }
 
 func ThrowError(text string, code uint64, err error) {
 	var errorInfo_ = ErrorInfo{text, code, nil,err}
-	p_logger.Logger.Error(fmt.Sprintf(`ERROR: %v`, errorInfo_))
 	panic(errorInfo_)
 }
 
 func ThrowErrorWithData(text string, code uint64, data interface{}, err error) {
 	var errorInfo_ = ErrorInfo{text, code, data,err}
-	p_logger.Logger.Error(fmt.Sprintf(`ERROR: %v`, errorInfo_))
 	panic(errorInfo_)
 }
 
--- go.mod
+++ go.mod
@@ -1,3 +1,1 @@
 module github.com/pefish/go-error
-
-require github.com/pefish/go-logger v0.0.2
