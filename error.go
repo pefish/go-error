e6381592cabf5c49defa6936dbc1b7bd85a37245
joy <joy.lai@zg.com>
2019-09-25 12:59:18 +0800

    fix

--- error.go
+++ error.go
@@ -1,5 +1,7 @@
 package go_error
 
+import "fmt"
+
 type ErrorInfo struct {
 	InternalErrorMessage string // 开发人员在测试环境才能看到的消息
 	ErrorMessage         string // 用户也可以看到的消息，不可以带敏感信息
@@ -14,22 +16,17 @@ var (
 	INTERNAL_ERROR_CODE uint64 = 1
 )
 
-func (e *ErrorInfo) Error() string {
-	return e.ErrorMessage
+func (this *ErrorInfo) Error() string {
+	return fmt.Sprintf(`internal_msg: %s, msg: %s`, this.InternalErrorMessage, this.ErrorMessage)
 }
 
 func Recover(fun func(msg string, internalMsg string, code uint64, data interface{}, err interface{})) {
 	if err := recover(); err != nil {
-		if _, ok := err.(ErrorInfo); !ok {
-			msg := ``
-			if _, ok := err.(error); !ok {
-				msg = err.(string)
-			} else {
-				msg = err.(error).Error()
-			}
+		if _, ok := err.(*ErrorInfo); !ok {
+			msg := fmt.Sprint(err)
 			fun(msg, msg, INTERNAL_ERROR_CODE, nil, err)
 		} else {
-			errorInfoStruct := err.(ErrorInfo)
+			errorInfoStruct := err.(*ErrorInfo)
 			fun(errorInfoStruct.ErrorMessage, errorInfoStruct.InternalErrorMessage, errorInfoStruct.ErrorCode, errorInfoStruct.Data, err)
 		}
 	}
@@ -37,60 +34,60 @@ func Recover(fun func(msg string, internalMsg string, code uint64, data interfac
 
 func ThrowInternalWithInternalMsg(msg string, internalMsg string) {
 	var errorInfo_ = ErrorInfo{internalMsg, msg, INTERNAL_ERROR_CODE, nil, nil}
-	panic(errorInfo_)
+	panic(&errorInfo_)
 }
 
 func ThrowInternal(text string) {
 	var errorInfo_ = ErrorInfo{text, text, INTERNAL_ERROR_CODE, nil, nil}
-	panic(errorInfo_)
+	panic(&errorInfo_)
 }
 
 func ThrowInternalError(text string, err interface{}) {
 	var errorInfo_ = ErrorInfo{text, text, INTERNAL_ERROR_CODE, nil, err}
-	panic(errorInfo_)
+	panic(&errorInfo_)
 }
 
 func ThrowInternalErrorWithInternalMsg(msg string, internalMsg string, err interface{}) {
 	var errorInfo_ = ErrorInfo{internalMsg, msg, INTERNAL_ERROR_CODE, nil, err}
-	panic(errorInfo_)
+	panic(&errorInfo_)
 }
 
 func Throw(text string, code uint64) {
 	var errorInfo_ = ErrorInfo{text, text, code, nil, nil}
-	panic(errorInfo_)
+	panic(&errorInfo_)
 }
 
 func ThrowWithInternalMsg(msg string, internalMsg string, code uint64) {
 	var errorInfo_ = ErrorInfo{internalMsg, msg, code, nil, nil}
-	panic(errorInfo_)
+	panic(&errorInfo_)
 }
 
 func ThrowWithData(text string, code uint64, data interface{}) {
 	var errorInfo_ = ErrorInfo{text, text, code, data, nil}
-	panic(errorInfo_)
+	panic(&errorInfo_)
 }
 
 func ThrowWithDataInternalMsg(msg string, internalMsg string, code uint64, data interface{}) {
 	var errorInfo_ = ErrorInfo{internalMsg, msg, code, data, nil}
-	panic(errorInfo_)
+	panic(&errorInfo_)
 }
 
 func ThrowError(text string, code uint64, err interface{}) {
 	var errorInfo_ = ErrorInfo{text, text, code, nil, err}
-	panic(errorInfo_)
+	panic(&errorInfo_)
 }
 
 func ThrowErrorWithInternalMsg(msg string, internalMsg string, code uint64, err interface{}) {
 	var errorInfo_ = ErrorInfo{internalMsg, msg, code, nil, err}
-	panic(errorInfo_)
+	panic(&errorInfo_)
 }
 
 func ThrowErrorWithData(text string, code uint64, data interface{}, err interface{}) {
 	var errorInfo_ = ErrorInfo{text, text, code, data, err}
-	panic(errorInfo_)
+	panic(&errorInfo_)
 }
 
 func ThrowErrorWithDataInternalMsg(msg string, internalMsg string, code uint64, data interface{}, err interface{}) {
 	var errorInfo_ = ErrorInfo{internalMsg, msg, code, data, err}
-	panic(errorInfo_)
+	panic(&errorInfo_)
 }
--- test/index.go
+++ test/index.go
@@ -1,17 +1,18 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"github.com/pefish/go-error"
 )
 
 func main() {
 	defer go_error.Recover(func(msg string, internalMsg string, code uint64, data interface{}, err interface{}) {
-		fmt.Println(msg, internalMsg, code, data, err)
+		fmt.Println(msg, internalMsg, code, data, fmt.Sprint(err))
 	})
 
-	go_error.ThrowErrorWithData(`test`, 12, map[string]interface{}{
-		`a`: 1,
-	}, errors.New(`haha`))
+	//go_error.ThrowErrorWithData(`test`, 12, map[string]interface{}{
+	//	`a`: 1,
+	//}, errors.New(`haha`))
+	panic(`111`)
+	//panic(errors.New(`haha`))
 }
