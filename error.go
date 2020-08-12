2524338ae8267e782e252b5cb71b01d51f0cea44
pefish <pefish@qq.com>
2020-08-12 14:02:49 +0800

    Improve

--- error.go
+++ error.go
@@ -1,13 +1,15 @@
 package go_error
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 type ErrorInfo struct {
-	InternalErrorMessage string // 开发人员在测试环境才能看到的消息
-	ErrorMessage         string // 用户也可以看到的消息，不可以带敏感信息
-	ErrorCode            uint64
-	Data                 interface{}
-	Err                  interface{}
+	Msg  string      `json:"msg"`
+	Code uint64      `json:"code"`
+	Data interface{} `json:"data"`
+	Err  error       `json:"err,omitempty"`
 }
 
 // 供外部使用
@@ -16,82 +18,64 @@ var (
 	INTERNAL_ERROR_CODE uint64 = 1
 )
 
-func (this *ErrorInfo) String() string {
-	return fmt.Sprintf(`ErrorInfo -> internal_msg: %s, msg: %s, error_code: %d, data: %#v, err: %#v`, this.InternalErrorMessage, this.ErrorMessage, this.ErrorCode, this.Data, this.Err)
+func (ei *ErrorInfo) String() string {
+	return fmt.Sprintf(`ErrorInfo -> msg: %s, code: %d, data: %#v, err: %#v`, ei.Msg, ei.Code, ei.Data, ei.Err)
 }
 
-func (this *ErrorInfo) Error() string {
-	return this.String()
+func (ei *ErrorInfo) Error() string {
+	return ei.String()
 }
 
-func Recover(fun func(msg string, internalMsg string, code uint64, data interface{}, err interface{})) {
+func (ei *ErrorInfo) Unwrap() error {
+	return ei.Err
+}
+
+func Recover(fun func(*ErrorInfo)) {
 	if err := recover(); err != nil {
-		if _, ok := err.(*ErrorInfo); !ok {
+		switch e := err.(type) {
+		case *ErrorInfo:
+			fun(e)
+		case error:
+			fun(WrapWithAll(e.Error(), INTERNAL_ERROR_CODE, nil, e))
+		default:
 			msg := fmt.Sprint(err)
-			fun(``, msg, INTERNAL_ERROR_CODE, nil, err)
-		} else {
-			errorInfoStruct := err.(*ErrorInfo)
-			fun(errorInfoStruct.ErrorMessage, errorInfoStruct.InternalErrorMessage, errorInfoStruct.ErrorCode, errorInfoStruct.Data, errorInfoStruct.Err)
+			fun(WrapWithAll(msg, INTERNAL_ERROR_CODE, nil, errors.New(msg)))
 		}
 	}
 }
 
-func ThrowInternalWithInternalMsg(msg string, internalMsg string) {
-	var errorInfo_ = ErrorInfo{internalMsg, msg, INTERNAL_ERROR_CODE, nil, nil}
-	panic(&errorInfo_)
-}
-
 func ThrowInternal(text string) {
-	var errorInfo_ = ErrorInfo{text, text, INTERNAL_ERROR_CODE, nil, nil}
+	var errorInfo_ = ErrorInfo{text, INTERNAL_ERROR_CODE, nil, nil}
 	panic(&errorInfo_)
 }
 
-func ThrowInternalError(text string, err interface{}) {
-	var errorInfo_ = ErrorInfo{text, text, INTERNAL_ERROR_CODE, nil, err}
+func ThrowInternalWithError(text string, err error) {
+	panic(WrapWithErr(text, err))
+}
+
+func ThrowWithCode(text string, code uint64) {
+	var errorInfo_ = ErrorInfo{text, code, nil, nil}
 	panic(&errorInfo_)
 }
 
-func ThrowInternalErrorWithInternalMsg(msg string, internalMsg string, err interface{}) {
-	var errorInfo_ = ErrorInfo{internalMsg, msg, INTERNAL_ERROR_CODE, nil, err}
+func ThrowWithCodeAndData(text string, code uint64, data error) {
+	var errorInfo_ = ErrorInfo{text, code, data, nil}
 	panic(&errorInfo_)
 }
 
-func Throw(text string, code uint64) {
-	var errorInfo_ = ErrorInfo{text, text, code, nil, nil}
+func ThrowWithCodeAndErr(text string, code uint64, err error) {
+	var errorInfo_ = ErrorInfo{text, code, nil, err}
 	panic(&errorInfo_)
 }
 
-func ThrowWithInternalMsg(msg string, internalMsg string, code uint64) {
-	var errorInfo_ = ErrorInfo{internalMsg, msg, code, nil, nil}
-	panic(&errorInfo_)
+func ThrowWithAll(text string, code uint64, data interface{}, err error) {
+	panic(WrapWithAll(text, code, data, err))
 }
 
-func ThrowWithData(text string, code uint64, data interface{}) {
-	var errorInfo_ = ErrorInfo{text, text, code, data, nil}
-	panic(&errorInfo_)
+func WrapWithAll(text string, code uint64, data interface{}, err error) *ErrorInfo {
+	return &ErrorInfo{text, code, data, err}
 }
 
-func ThrowWithDataInternalMsg(msg string, internalMsg string, code uint64, data interface{}) {
-	var errorInfo_ = ErrorInfo{internalMsg, msg, code, data, nil}
-	panic(&errorInfo_)
-}
-
-func ThrowError(text string, code uint64, err interface{}) {
-	var errorInfo_ = ErrorInfo{text, text, code, nil, err}
-	panic(&errorInfo_)
-}
-
-func ThrowErrorWithInternalMsg(msg string, internalMsg string, code uint64, err interface{}) {
-	var errorInfo_ = ErrorInfo{internalMsg, msg, code, nil, err}
-	panic(&errorInfo_)
-}
-
-func ThrowErrorWithData(text string, code uint64, data interface{}, err interface{}) {
-	var errorInfo_ = ErrorInfo{text, text, code, data, err}
-	panic(&errorInfo_)
-}
-
-func ThrowErrorWithDataInternalMsg(msg string, internalMsg string, code uint64, data interface{}, err interface{}) {
-	var errorInfo_ = ErrorInfo{internalMsg, msg, code, data, err}
-	panic(&errorInfo_)
+func WrapWithErr(text string, err error) *ErrorInfo {
+	return &ErrorInfo{text, INTERNAL_ERROR_CODE, nil, err}
 }
--- error_test.go
+++ error_test.go
@@ -1,10 +1,34 @@
 package go_error
 
 import (
+	"errors"
 	"fmt"
+	"github.com/pefish/go-test-assert"
 	"testing"
 )
 
 func TestErrorInfo_Test(t *testing.T) {
-	fmt.Println(12)
+	defer Recover(func(errorInfo *ErrorInfo) {
+		test.Equal(t, `haha`, errorInfo.Msg)
+		test.Equal(t, true, errorInfo.Code == 1000)
+		test.Equal(t, `hehe`, errorInfo.Err.Error())
+	})
+	test.Equal(t, `ErrorInfo -> msg: post error, code: 1, data: <nil>, err: &errors.errorString{s:"haha"}`, fmt.Sprint(&ErrorInfo{
+		Code: INTERNAL_ERROR_CODE,
+		Msg:  `post error`,
+		Err:  errors.New(`haha`),
+	}))
+	ThrowWithCodeAndErr(`haha`, 1000, errors.New(`hehe`))
+}
+
+func TestErrorInfo_Test1(t *testing.T) {
+	defer Recover(func(errorInfo *ErrorInfo) {
+		test.Equal(t, `interface conversion: interface {} is string, not float64`, errorInfo.Msg)
+		test.Equal(t, true, errorInfo.Code == 1)
+		test.Equal(t, `interface conversion: interface {} is string, not float64`, errorInfo.Err.Error())
+	})
+
+	var a interface{} = `aa`
+	b := a.(float64)
+	fmt.Println(b)
 }
