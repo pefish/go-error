33fd23b256813f42a452b6e20e932d45b2b54727
pefish <darrondarron@qq.com>
2024-01-25 15:51:02 +0800

    v1.0.8

--- error_test.go
+++ error_test.go
@@ -2,14 +2,14 @@ package go_error
 
 import (
 	"fmt"
-	"github.com/pefish/go-test-assert"
+	go_test_ "github.com/pefish/go-test"
 	"testing"
 )
 
 func TestErrorInfo_Test1(t *testing.T) {
 	defer Recover(func(errorInfo *ErrorInfo) {
-		test.Equal(t, true, errorInfo.Code == 1)
-		test.Equal(t, `interface conversion: interface {} is string, not float64`, errorInfo.Err.Error())
+		go_test_.Equal(t, true, errorInfo.Code == 1)
+		go_test_.Equal(t, `interface conversion: interface {} is string, not float64`, errorInfo.Err.Error())
 	})
 
 	var a interface{} = `aa`
--- go.mod
+++ go.mod
@@ -2,7 +2,6 @@ module github.com/pefish/go-error
 
 go 1.20
 
-require (
-	github.com/pefish/go-test-assert v0.0.1
-	github.com/pkg/errors v0.9.1
-)
+require github.com/pkg/errors v0.9.1
+
+require github.com/pefish/go-test v0.0.4
